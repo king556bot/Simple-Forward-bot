38fc0bde2b41206f6fb7132eef796b821a143d97
king556bot <chiragbhatt479@gmail.com>
2025-07-23 12:33:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from os import environ
 API_ID = int(environ.get("API_ID", "25049176"))
 API_HASH = environ.get("API_HASH", "7729713683:AAGOEtndJw9_Ao0HE-7NlVcBHV9VLFy0lhM")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7616359279:AAGMB2Pct7rHQBuTvh6W3ris8SFvrMhNbAg")
-MONGO_URI = environ.get("MONGO_URI", "")
+MONGO_URI = environ.get("MONGO_URI", "mongodb+srv://chiragbhatt479:@chirag.zdokauz.mongodb.net/?retryWrites=true&w=majority&appName=Chirag")
 OWNER_ID = int(environ.get("OWNER_ID", "7146651806"))
