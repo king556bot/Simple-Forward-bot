e74777c6976444f2234e871278d2875f4340e6c0
king556bot <chiragbhatt479@gmail.com>
2025-07-23 12:06:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "25049176"))
+API_HASH = environ.get("API_HASH", "7729713683:AAGOEtndJw9_Ao0HE-7NlVcBHV9VLFy0lhM")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7616359279:AAGMB2Pct7rHQBuTvh6W3ris8SFvrMhNbAg")
 MONGO_URI = environ.get("MONGO_URI", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
+OWNER_ID = int(environ.get("OWNER_ID", "7146651806"))
